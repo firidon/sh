3d23ef5bc745b0799588fdb2175949e273aec95b
FI Ridon <37545783+firidon@users.noreply.github.com>
2020-05-31 20:53:33 +0600

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 apt-get install figlet -yq --silent
 
-
+rm -r $PREFIX/etc/motd
+rm  $PREFIX/etc/bash.bashrc
+touch $PREFIX/etc/bash.bashrc
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Type your termux name: \e[0m' name
+
+echo" if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not> 
+        command_not_found_handle() {
+                /data/data/com.termux/files/usr/libexec/termux/com>
+        }
+fi
+
+PS1='\$ ' " >> $PREFIX/etc/bash.bashrc
 
 echo "figlet $name" >> $PREFIX/etc/bash.bashrc
 
