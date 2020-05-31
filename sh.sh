f9cbe8bdee6fd35ca39245978cf9c711db26b767
FI Ridon <37545783+firidon@users.noreply.github.com>
2020-05-31 19:43:13 +0600

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get install figlet -yq --silent
-apt-get install toilet -yq --silent
+
 
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Type your termux name: \e[0m' name
 
-echo "toilet -F metal -F border -f future $name" >> $PREFIX/etc/bash.bashrc
+echo "figlet $name $name" >> $PREFIX/etc/bash.bashrc
 
 printf "[*] Now open new session & enjoy"
