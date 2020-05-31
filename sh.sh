07aa45162c46c75d4b11c5c12b771d6e91f6115e
FI Ridon <37545783+firidon@users.noreply.github.com>
2020-05-31 20:49:36 +0600

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -4,6 +4,6 @@ apt-get install figlet -yq --silent
 
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Type your termux name: \e[0m' name
 
-echo "figlet $name $name" >> $PREFIX/etc/bash.bashrc
+echo "figlet $name" >> $PREFIX/etc/bash.bashrc
 
 printf "[*] Now open new session & enjoy"
