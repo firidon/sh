ae40f69e8befac7941c0877cdb8647a9ccd0abb0
FI Ridon <37545783+firidon@users.noreply.github.com>
2020-05-31 19:29:34 +0600

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install figlet -yq --silent
 
-read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] name: [Default is 1] \e[0m' name
+read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Type your termux name: \e[0m' name
 
 echo "figlet $name" >> $PREFIX/etc/bash.bashrc
 
