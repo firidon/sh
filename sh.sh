909b335c4520735eb8f95677f10c9cd4c73d1614
FI Ridon <37545783+firidon@users.noreply.github.com>
2020-05-31 20:56:52 +0600

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -6,13 +6,13 @@ rm  $PREFIX/etc/bash.bashrc
 touch $PREFIX/etc/bash.bashrc
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Type your termux name: \e[0m' name
 
-echo" if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not> 
+echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not> 
         command_not_found_handle() {
                 /data/data/com.termux/files/usr/libexec/termux/com>
         }
 fi
 
-PS1='\$ ' " >> $PREFIX/etc/bash.bashrc
+PS1='\$ '" >> $PREFIX/etc/bash.bashrc
 
 echo "figlet $name" >> $PREFIX/etc/bash.bashrc
 
